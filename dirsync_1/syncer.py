9a68cab15d8837f6a46a9d2b65c21152fc8563cd
Kapa Kudaibergenov <66877169+kudkapa59@users.noreply.github.com>
2022-12-19 13:32:04 +0100

    working version

--- dirsync_1/syncer.py
+++ dirsync_1/syncer.py
@@ -93,25 +93,19 @@ class Syncer(object):
         def get_option(name):
             return options.get(name, OPTIONS[name][1]['default'])
 
-        self._copydirection = 2 if get_option('twoway') else 0
+        self._copydirection = 0
         self._forcecopy = get_option('force')
-        self._maketarget = get_option('create')
         self._use_ctime = get_option('ctime')
         self._use_content = True
 
-        self._ignore = get_option('ignore')
-        self._only = get_option('only')
-        self._exclude = list(get_option('exclude'))
-        self._include = get_option('include')
-
         # excludes .dirsync file by default, must explicitly be in include
         # not to be excluded
-        self._exclude.append('^\.dirsync$')
+        # self._exclude.append('^\.dirsync$')
 
         if not os.path.isdir(self._dir1):
             raise ValueError("Error: Source directory does not exist.")
 
-        if not self._maketarget and not os.path.isdir(self._dir2):
+        if not os.path.isdir(self._dir2):
             raise ValueError(
                 "Error: Target directory %s does not exist. "
                 "(Try the -c or --create option to create it)." % self._dir2)
@@ -127,40 +121,40 @@ class Syncer(object):
 
         self._numdirs += 1
 
-        excl_patterns = set(self._exclude).union(self._ignore)
+        # excl_patterns = set(self._exclude).union(self._ignore)
 
         for cwd, dirs, files in os.walk(dir1):
             self._numdirs += len(dirs)
             for f in dirs + files:
                 path = os.path.relpath(os.path.join(cwd, f), dir1)
                 re_path = path.replace('\\', '/')
-                if self._only:
-                    for pattern in self._only:
-                        if re.match(pattern, re_path):
-                            # go to exclude and ignore filtering
-                            break
-                    else:
-                        # next item, this one does not match any pattern
-                        # in the _only list
-                        continue
+                # if self._only:
+                #     for pattern in self._only:
+                #         if re.match(pattern, re_path):
+                #             # go to exclude and ignore filtering
+                #             break
+                #     else:
+                #         # next item, this one does not match any pattern
+                #         # in the _only list
+                #         continue
 
                 add_path = False
-                for pattern in self._include:
-                    if re.match(pattern, re_path):
-                        add_path = True
-                        break
-                else:
-                    # path was not in includes
-                    # test if it is in excludes
-                    for pattern in excl_patterns:
-                        if re.match(pattern, re_path):
-                            # path is in excludes, do not add it
-                            break
-                    else:
-                        # path was not in excludes
-                        # it should be added
-                        add_path = True
-
+                # for pattern in self._include:
+                #     if re.match(pattern, re_path):
+                #         add_path = True
+                #         break
+                # else:
+                #     # path was not in includes
+                #     # test if it is in excludes
+                #     for pattern in excl_patterns:
+                #         if re.match(pattern, re_path):
+                #             # path is in excludes, do not add it
+                #             break
+                #     else:
+                #         # path was not in excludes
+                #         # it should be added
+                #         add_path = True
+                add_path = True
                 if add_path:
                     left.add(path)
                     anc_dirs = re_path[:-1].split('/')
@@ -173,17 +167,17 @@ class Syncer(object):
             for f in dirs + files:
                 path = os.path.relpath(os.path.join(cwd, f), dir2)
                 re_path = path.replace('\\', '/')
-                for pattern in self._ignore:
-                    if re.match(pattern, re_path):
-                        if f in dirs:
-                            dirs.remove(f)
-                        break
-                else:
-                    right.add(path)
+                # for pattern in self._ignore:
+                #     if re.match(pattern, re_path):
+                #         if f in dirs:
+                #             dirs.remove(f)
+                #         break
+                # else:
+                right.add(path)
                     # no need to add the parent dirs here,
                     # as there is no _only pattern detection
-                    if f in dirs and path not in left:
-                        self._numdirs += 1
+                if f in dirs and path not in left:
+                    self._numdirs += 1
 
         common = left.intersection(right)
         left.difference_update(common)
@@ -196,16 +190,16 @@ class Syncer(object):
 
         self._starttime = time.time()
 
-        if not os.path.isdir(self._dir2):
-            if self._maketarget:
-                # if self._verbose:
-                self.log('Creating directory %s' % self._dir2)
-                try:
-                    os.makedirs(self._dir2)
-                    self._numnewdirs += 1
-                except Exception as e:
-                    self.log(str(e))
-                    return None
+        # if not os.path.isdir(self._dir2):
+            # if self._maketarget:
+            #     # if self._verbose:
+            #     self.log('Creating directory %s' % self._dir2)
+            #     try:
+            #         os.makedirs(self._dir2)
+            #         self._numnewdirs += 1
+            #     except Exception as e:
+            #         self.log(str(e))
+            #         return None
 
         # All right!
         # self._mainfunc()
