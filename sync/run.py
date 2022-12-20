89048b0e7b56d2ea9b05832523990da0e50705ae
Kapa Kudaibergenov <66877169+kudkapa59@users.noreply.github.com>
2022-12-20 15:52:10 +0100

    Removed options.py (can be returned when we need some info for deploying package), removed some useless code

--- sync/run.py
+++ sync/run.py
@@ -2,39 +2,10 @@
 """
 
 from __future__ import print_function
-# from apscheduler.schedulers.background import BackgroundScheduler
-
-import sys
-import os
-import time
-import schedule
-
 from .syncer import Syncer
-# sched = BackgroundScheduler()
+
 
 def sync(sourcedir, targetdir, interval, log_file_path):
     copier = Syncer(sourcedir, targetdir, interval, log_file_path)
     copier.do_work()
-
-    # print report at the end
     copier.report()
-
-    return set(copier._changed).union(copier._added).union(copier._deleted)
-
-
-def from_cmdline():
-    from .options import ArgParser, USER_CFG_FILE, DEFAULT_USER_CFG
-
-    # create config file if it does not exist
-    user_cfg_file = os.path.expanduser(USER_CFG_FILE)
-    if not os.path.isfile(user_cfg_file):
-        print('Creating user config file "%s" ...' % user_cfg_file, end=''),
-        with open(user_cfg_file, 'w') as f:
-            f.write(DEFAULT_USER_CFG)
-        print(' Done')
-
-    try:
-        sync(**vars(ArgParser().parse_args()))
-    except Exception as e:
-        sys.stdout.write(str(e) + '\n')
-        sys.exit(2)
