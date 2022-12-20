f953f83c66f49ac5c95c9d43664c448e88e33528
Kapa Kudaibergenov <66877169+kudkapa59@users.noreply.github.com>
2022-12-20 15:20:47 +0100

    Made help instructions

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
 from sync import sync
-import logging
-import os
-import sys
 import schedule
-import time
-
 import argparse
 
 parser = argparse.ArgumentParser(
     description='''This is a Python package that synchronizes two folders: source and replica. The
-program maintains a full, identical copy of source folder at replica folder.''',
-    epilog="""All is well that ends well.""")
-# parser.add_argument('--foo', type=int, default=42, help='FOO!')
-parser.add_argument('sourcedir', nargs='*', help='Location of the source folder.')
-parser.add_argument('targetdir', nargs='*', help='Location of the replica folder. If the folder doesn\'t exist at this directory, '
-                                                 'it will be created.')
-parser.add_argument('interval', type=float, nargs='*', help='Synchronization interval. Stays for with what frequency synchronization'
-                                                'should be repeated. Float argument.')
-parser.add_argument('time_units', nargs='*', help='Time measurement units. Possible values: s for seconds, m for minutes,'
-                                                    'h for hours')
-parser.add_argument('log_file_path', nargs='*', help='Log file path. If the file doesn\'t exist, '
-                                                       'it will be created.')
-args=parser.parse_args()
+program maintains a full, identical copy of source folder at replica folder.''')
+parser.add_argument('sourcedir', help='Location of the source folder.')
+parser.add_argument('targetdir', help='Location of the replica folder. If the folder doesn\'t exist at this directory, '
+                                      'it will be created.')
+parser.add_argument('interval', type=float,
+                    help='Synchronization interval. Stays for with what frequency synchronization'
+                         'should be repeated. Float argument.')
+parser.add_argument('--time_units', choices=['s', 'm', 'h'],
+                    help='Time measurement units. Possible values: s for seconds, m for minutes,'
+                         'h for hours. Default value is seconds', default='s')
+parser.add_argument('log_file_path', help='Log file path. If the file doesn\'t exist, '
+                                          'it will be created.')
+args = parser.parse_args()
 
-# parser.add_argument('--name', type=str, required=True)
-# # Parse the argument
-# args = parser.parse_args()
-# # Print "Hello" + the user input argument
-# print('Hello,', args.name)
+sync(args.sourcedir, args.targetdir, str(args.interval) + args.time_units, args.log_file_path)
 
-sourcedir = sys.argv[1]
-targetdir = sys.argv[2]
-interval = float(sys.argv[3])
-time_units = sys.argv[4]
-log_file_path = sys.argv[5]
-
-sync(sourcedir, targetdir, str(interval) + time_units,  log_file_path)
-
-if time_units == 's':
-    schedule.every(interval).seconds.do(sync, sourcedir,
-                                        targetdir, str(interval) + time_units, log_file_path)
-elif time_units == 'm':
-    schedule.every(interval).minutes.do(sync, sourcedir,
-                                        targetdir, str(interval) + time_units, log_file_path)
-elif time_units == 'h':
-    schedule.every(interval).hours.do(sync, sourcedir,
-                                        targetdir, str(interval) + time_units, log_file_path)
-else:
-    raise ValueError("Not suitable time units")
+if args.time_units == 's':
+    schedule.every(args.interval).seconds.do(sync, args.sourcedir,
+                                             args.targetdir, str(args.interval) + args.time_units, args.log_file_path)
+elif args.time_units == 'm':
+    schedule.every(args.interval).minutes.do(sync, args.sourcedir,
+                                             args.targetdir, str(args.interval) + args.time_units, args.log_file_path)
+elif args.time_units == 'h':
+    schedule.every(args.interval).hours.do(sync, args.sourcedir,
+                                           args.targetdir, str(args.interval) + args.time_units, args.log_file_path)
 
 while 1:
     schedule.run_pending()
-
